612668f6b8552ee8278a2e2350a5791f265b2ed4
odyssey-D <danwillh@gmail.com>
2026-01-21 13:50:50 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-import { Header } from "@/components/Header";
-import { Hero } from "@/components/Hero";
-import { Outcomes } from "@/components/Outcomes";
-import { Modules } from "@/components/Modules";
-import { SiteIntelligence } from "@/components/SiteIntelligence";
-import { Segments } from "@/components/Segments";
-import { WaitlistForm } from "@/components/WaitlistForm";
-import { FAQ } from "@/components/FAQ";
-import { Footer } from "@/components/Footer";
-
-export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen pt-16">
-        <Hero />
-        <Outcomes />
-        <Modules />
-        <SiteIntelligence />
-        <Segments />
-        <WaitlistForm />
-        <FAQ />
-        <Footer />
-      </main>
-    </>
-  );
-}
+{/* <Hero /> */}
+{/* <Outcomes /> */}
+{/* <Modules /> */}
+{/* <SiteIntelligence /> */}
+<Segments />
